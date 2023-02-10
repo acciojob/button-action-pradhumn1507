4a659dde1728f7848fd68aff8e9600c939a7e222
pradhumn1507 <PRADHUMN1507@GMAIL.COM>
2023-02-10 16:05:25 +0530

    done

--- src/components/App.js
+++ src/components/App.js
@@ -5,7 +5,8 @@ const App = (props) => {
 
   return (
     <div className="App" id="main">
-      // Do not alter the main div
+     <p id="para">"Hello, I've learnt to use the full-stack evaluation tool. This makes me so happy"</p>
+     <button id="click"></button>
     </div>
   );
 }
