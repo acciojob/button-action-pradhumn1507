6e9917fdb0aca0f619517e1684b3221a9b5fe6d1
pradhumn1507 <117638511+pradhumn1507@users.noreply.github.com>
2023-02-14 15:21:10 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -2,13 +2,19 @@ import React, { useState } from "react";
 import './../styles/App.css';
 
 const App = (props) => {
+  const [flag, setFlag] = useState(false);
 
   return (
     <div className="App" id="main">
-     <p id="para">"Hello, I've learnt to use the full-stack evaluation tool. This makes me so happy"</p>
-     <button id="click"></button>
+      <p id="para" className={flag ? "show" : "hide"}>
+        Hello, I've learnt to use the full-stack evaluation tool. This makes me
+        so happy
+      </p>
+      <button id="click" onClick={() => setFlag(!flag)}>
+        Click
+      </button>
     </div>
   );
 }
 
-export default App
+export default App;
